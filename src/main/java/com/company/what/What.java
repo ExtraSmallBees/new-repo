595a1f6ec85bd6c331c72a7c38a938615987753c
ExtraSmallBees <144105366+ExtraSmallBees@users.noreply.github.com>
2023-09-12 11:51:29 +0100

    test

---
+++ src/main/java/com/company/what/What.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.company.what;
+
+/**
+ *
+ * @author epict
+ */
+public class What {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
